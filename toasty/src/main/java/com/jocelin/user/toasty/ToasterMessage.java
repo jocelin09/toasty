2bd61d0cf9c8494018129fb2a37b049173d14aaf
Jocelin Thomas <jocelinthomas177@gmail.com>
2019-11-06 15:51:06 +0530

    Initial Commit

---
+++ toasty/src/main/java/com/jocelin/user/toasty/ToasterMessage.java
@@ -0,0 +1,13 @@
+package com.jocelin.user.toasty;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterMessage {
+
+    public static void message(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
